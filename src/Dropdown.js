d9ca5784df2631214654cf57f77a63dee17ccad1
andyJiayiLi <lijiayi606@gmail.com>
2022-09-23 19:10:24 -0700

    multiple choices implemented

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -6,37 +6,45 @@ import { faSquare, faCheckSquare, faSquareCaretUp, faSquareCaretDown } from '@fo
 export default class Dropdown extends Component {
     constructor(props){
         super(props);
+        let arr = new Array(props.options.length).fill(false)
         this.state = {
-            value: '',
-            showOptions: false
+            value: arr,
+            options: props.options,
+            showOptions: false,
+            multiselect: false
         }
+        this.selectOption = this.selectOption.bind(this)
+        this.generateText = this.generateText.bind(this)
     }
 
     selectOption(key) {
-        this.setState({value:key})
+        let value = this.state.value
+        value[key] = !value[key]
+        console.log(value)
+        this.setState({value})
     }
 
-    buildList(options) {
+    buildList() {
         return (<div>
-            {options.map((item) =>
-                <Item key={item} value={item} onClick={()=>this.selectOption(item)} isSelected={item===this.state.value}/>
+            {this.state.options.map((item, index) =>
+                <Item key={item} value={item} onClick={()=>this.selectOption(index)} isSelected={this.state.value[index]}/>
             )}
         </div>)
-
-
+    }
+    generateText() {
+        return this.state.options.filter((value, index) => this.state.value[index]).join(", ")
     }
     render() {
-        let {options, multiselect = false} = this.props;
         return(
         <div className='dropmenu'>
             <div className='dropbox' onClick={()=>{this.setState({showOptions: !this.state.showOptions})}}>
-                <div className='selectedText'>{this.state.value}</div>
+                <div className='selectedText'>{this.generateText()}</div>
                 {this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretUp}/>}
                 {!this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretDown}/>}
             </div>
             {this.state.showOptions &&
             <div className='droplist'>
-                {this.buildList(options)}
+                {this.buildList()}
             </div>
             }
         </div>

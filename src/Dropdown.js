56815f04d9b233fbe9e235bfd234efa79021b31d
andyJiayiLi <lijiayi606@gmail.com>
2022-09-23 18:54:11 -0700

    single choice implemented

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -7,16 +7,19 @@ export default class Dropdown extends Component {
     constructor(props){
         super(props);
         this.state = {
-            value: [],
+            value: '',
             showOptions: false
         }
     }
 
+    selectOption(key) {
+        this.setState({value:key})
+    }
 
     buildList(options) {
         return (<div>
             {options.map((item) =>
-                <Item key={item} value={item}/>
+                <Item key={item} value={item} onClick={()=>this.selectOption(item)} isSelected={item===this.state.value}/>
             )}
         </div>)
 
@@ -26,13 +29,16 @@ export default class Dropdown extends Component {
         let {options, multiselect = false} = this.props;
         return(
         <div className='dropmenu'>
-            <div className='dropbox'>
+            <div className='dropbox' onClick={()=>{this.setState({showOptions: !this.state.showOptions})}}>
                 <div className='selectedText'>{this.state.value}</div>
-                <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretUp} />
+                {this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretUp}/>}
+                {!this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretDown}/>}
             </div>
+            {this.state.showOptions &&
             <div className='droplist'>
                 {this.buildList(options)}
             </div>
+            }
         </div>
         );
     }
@@ -40,8 +46,9 @@ export default class Dropdown extends Component {
 
 function Item(props) {
     return (
-      <div className='optionRow' onClick={()=>{}}>
-        <FontAwesomeIcon icon={faSquare} />
+      <div className='optionRow' onClick={props.onClick}>
+        {!props.isSelected && <FontAwesomeIcon icon={faSquare} />}
+        {props.isSelected && <FontAwesomeIcon icon={faCheckSquare} />}
         <div className='optionText'>{props.value}</div>
       </div>
     )

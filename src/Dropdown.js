6c671e9f75b827cb4875b73019991181abc03ff6
andyJiayiLi <lijiayi606@gmail.com>
2022-09-23 19:39:47 -0700

    modified css

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,48 +1,57 @@
 import React, {Component} from 'react';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare, faCheckSquare, faSquareCaretUp, faSquareCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faSquareCaretUp, faSquareCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 export default class Dropdown extends Component {
     constructor(props){
         super(props);
         let arr = new Array(props.options.length).fill(false)
         this.state = {
-            value: arr,
+            choices: arr, // for multiselection
+            value: '', // for single selection
             options: props.options,
-            showOptions: false,
-            multiselect: false
+            multiselect: props.multiselect,
+            showOptions: false
         }
         this.selectOption = this.selectOption.bind(this)
         this.generateText = this.generateText.bind(this)
     }
 
-    selectOption(key) {
-        let value = this.state.value
-        value[key] = !value[key]
-        console.log(value)
-        this.setState({value})
+    selectOption(index, value) {
+        if(this.state.multiselect) {
+            let choices = this.state.choices
+            choices[index] = !choices[index]
+            this.setState({choices})
+        } else {
+            this.setState({value})
+        }
+
     }
 
     buildList() {
+        const {options, multiselect, choices, value} = this.state
         return (<div>
-            {this.state.options.map((item, index) =>
-                <Item key={item} value={item} onClick={()=>this.selectOption(index)} isSelected={this.state.value[index]}/>
+            {options.map((item, index) =>
+                <Item key={index} choices={item} multiselect={multiselect} onClick={()=>this.selectOption(index, item)} 
+                isSelected={multiselect ? choices[index] : value===item}/>
             )}
         </div>)
     }
     generateText() {
-        return this.state.options.filter((value, index) => this.state.value[index]).join(", ")
+        if(!this.state.multiselect) return this.state.value
+        return this.state.options.filter((choices, index) => this.state.choices[index]).join(", ")
     }
     render() {
+        const {showOptions} = this.state
         return(
         <div className='dropmenu'>
-            <div className='dropbox' onClick={()=>{this.setState({showOptions: !this.state.showOptions})}}>
+            <div className='dropbox' onClick={()=>{this.setState({showOptions: !showOptions})}}>
                 <div className='selectedText'>{this.generateText()}</div>
-                {this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretUp}/>}
-                {!this.state.showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretDown}/>}
+                {showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretUp}/>}
+                {!showOptions && <FontAwesomeIcon className='arrowIcon' icon={faSquareCaretDown}/>}
             </div>
-            {this.state.showOptions &&
+            {showOptions &&
             <div className='droplist'>
                 {this.buildList()}
             </div>
@@ -55,9 +64,8 @@ export default class Dropdown extends Component {
 function Item(props) {
     return (
       <div className='optionRow' onClick={props.onClick}>
-        {!props.isSelected && <FontAwesomeIcon icon={faSquare} />}
-        {props.isSelected && <FontAwesomeIcon icon={faCheckSquare} />}
-        <div className='optionText'>{props.value}</div>
+        {props.multiselect && props.isSelected && <FontAwesomeIcon className='icon' icon={faCheckSquare} />}
+        <div className='optionText'>{props.choices}</div>
       </div>
     )
   }
